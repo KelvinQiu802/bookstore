9b511392d5c7068d2a9d9538599db9460a181e73
KelvinQiu <kelvinqiu802@outlook.com>
2022-08-16 16:03:56 +0800

    feat: init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <a href='asd'>12</a>
+    </div>
+  );
+}
+
+export default App;
