54366b91cef7274121af1200ee539e285b0364a3
KelvinQiu <kelvinqiu802@outlook.com>
2022-08-17 13:59:34 +0800

    feat: rejected tip

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import { Box, createTheme, ThemeProvider } from '@mui/material';
+import {
+  Stack,
+  Box,
+  createTheme,
+  ThemeProvider,
+  Typography,
+} from '@mui/material';
 import './App.css';
 import Search from './features/Search/Search';
 import Loading from './features/Loading/Loading';
 import BookList from './features/BookList/BookList';
 import { useSelector } from 'react-redux';
+import { Wifi } from '@mui/icons-material';
 
 function App() {
   const status = useSelector((state) => state.search.status);
@@ -18,7 +25,18 @@ function App() {
       content = <BookList />;
       break;
     case 'rejected':
-      content = <h1>Rejected</h1>;
+      content = (
+        <Stack alignItems='center'>
+          <Typography variant='h6'>Request Failed</Typography>
+          <Wifi color='error' />
+          <Typography variant='p' sx={{ mt: 1 }}>
+            This site is using Google Book API
+          </Typography>
+          <Typography variant='p' sx={{ mt: 1 }}>
+            Please check your network connect.
+          </Typography>
+        </Stack>
+      );
   }
 
   const darkTheme = createTheme({
